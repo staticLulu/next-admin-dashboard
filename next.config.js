3c5276e0c8a6b32edbf5214954edbc18aaf11bd6
dangmakara <dang.dangmakara@gmail.com>
2024-11-21 17:14:54 +0700

    .

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 }
  
 module.exports = nextConfig
