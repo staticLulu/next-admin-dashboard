90127fc8127099b335b2be829d894ae79f51d026
dangmakara <dang.dangmakara@gmail.com>
2024-12-04 08:41:02 +0700

    .

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,5 +1,8 @@
+import DynamicButton from "@/components/DynamicButton";
 import AdminLayout from "@/Layouts/AdminLayout";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 
 type Props = {
@@ -10,15 +13,19 @@ type Props = {
 const UserAnalyticsDetailsPage = async ({params}: Props) => {
   const { id } = params;
   const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => res.json());
+
   return (
     <AdminLayout>
+      <Link href={'/users'}>
+        <DynamicButton label="back 🔙" variant="secondary"/>
+      </Link>
       <div className="flex justify-center my-5">
         <Image 
           src={"/helloworld2.jpg"} 
           alt="helloworld" 
           width={300} 
           height={300} 
-          className="shadow-slate-600 shadow-md rounded-2xl"
+          className="shadow-slate-600 shadow-sm"
         />
       </div>
       <div className="grid gap-5 grid-cols-3 border border-slate-200 p-5 shadow-sm">
